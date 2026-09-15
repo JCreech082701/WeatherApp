37741dc8165f4429b98fd39e81d94bde078e5515
JCreech082701 <W0523771@nscc.ca>
2026-09-15 14:37:45 -0300

    Testing

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello Git on $name!",
         modifier = modifier
     )
 }
